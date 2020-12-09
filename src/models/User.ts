b17bb2c27b18bc3f481e698e4207d0a225edda4d
ahmedaltaai <altaai.ahmed@gmx.at>
2020-12-09 22:50:55 +0100

    created user model

---
+++ src/models/User.ts
@@ -0,0 +1,33 @@
+import { Schema, model, Date, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date
+  }
+}
+
+const UserSchema: Schema = new Schema({
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+export default model<IUser>('User', UserSchema);
