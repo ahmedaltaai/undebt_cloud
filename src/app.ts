d3080efde2bcbc6e2b1ffd65fe31498ab882a6b6
ahmedaltaai <altaai.ahmed@gmx.at>
2020-12-09 22:53:10 +0100

    app is ready for take off

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,24 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
+import express, { Application, Request, Response, NextFunction, urlencoded } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+// init express
 const app: Application = express();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}.org1k.mongodb.net/test`, { useUnifiedTopology: true, useNewUrlParser: true }, 
+// connect to mongodb
+
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@undept.org1k.mongodb.net/undebt?retryWrites=true&w=majority`, 
+  { useUnifiedTopology: true, useNewUrlParser: true }, 
   () => console.log('connected to db!')
 );
+
+// body parser to json
+app.use(express.json());
 
 // register the user
 app.use('/register', require('./routes/register'));
 
-app.listen(process.env.PORT || 3000, () => console.log('Server started'))
+app.listen(process.env.PORT || 3000, () => console.log('Server started'));
