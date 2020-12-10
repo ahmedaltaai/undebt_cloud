c5e5b5324ee3958deffcb01e578a7b98a18cb918
ahmedaltaai <altaai.ahmed@gmx.at>
2020-12-10 21:50:43 +0100

    added password hashing function with bcrypt

--- src/controllers/register.ts
+++ src/controllers/register.ts
@@ -1,20 +1,26 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcryptjs';
 import User from '../models/User';
 
 
 export const registerUser = async (req: Request, res: Response) => {
 
+  // check if email exists in db
   const emailExists = await User.findOne({email: req.body.email});
   if(emailExists) return res.status(400).send('Email already exists');
-  
+
+  // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   const user = new User({
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
 
   try {
     const registerdUser = await user.save();
-    res.status(200).send(registerdUser);
+    res.status(200).send({user: user._id});
   } catch (err) {
     res.status(400).send(err);
   }
